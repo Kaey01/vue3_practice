d65fdb012e47289551c4222f226390d530654cb3
Kaey <wbforever@qq.com>
2023-06-07 22:50:43 +0800

    feat: 环境变量的配置

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,6 @@
   <div>app</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-      alias: {
-          "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
-      }
-  }
+    alias: {
+      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+    },
+  },
 })
